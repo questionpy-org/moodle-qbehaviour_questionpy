ec3fca8edff77d6df6ee5ca6e0c5d37908faf396
Maximilian Haye <m.haye@tu-berlin.de>
2024-03-28 17:45:35 +0100

    feat: implement custom qbehaviour_questionpy
    It informs the qtype_questionpy_question about the question attempt and pending step, and it enriches the response history with question, attempt and scoring states.

---
+++ behaviourtype.php
@@ -0,0 +1,27 @@
+<?php
+// This file is part of the QuestionPy Moodle plugin - https://questionpy.org
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * {@see question_behaviour_type} implementation stub for {@see qbehaviour_questionpy}.
+ *
+ * @package    qbehaviour_questionpy
+ * @author     Maximilian Haye
+ * @copyright  2024 TU Berlin, innoCampus {@link https://www.questionpy.org}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class qbehaviour_questionpy_type extends question_behaviour_type {
+    // FIXME: The question engine never tells us what the original question behaviour type is, so we can't delegate.
+}
