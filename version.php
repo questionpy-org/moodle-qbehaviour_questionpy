aee1e7395c268bdf2008c1f88a2ea2f57b54f376
Maximilian Haye <m.haye@tu-berlin.de>
2025-10-06 12:41:31 +0200

    chore: increase version and qtype dependency

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_questionpy';
-$plugin->version = 2025042800;
+$plugin->version = 2025100600;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.1';
 
 $plugin->dependencies = [
-    "qtype_questionpy" => 2025042800,
+    "qtype_questionpy" => 2025100600,
 ];
